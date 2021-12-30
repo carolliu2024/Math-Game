14892efafc743f734caa15bcddcd3af22436c344
carolliu2024 <75544065+carolliu2024@users.noreply.github.com>
2021-12-30 00:30:15 -0600

    Update controller.hxx

--- src/controller.hxx
+++ src/controller.hxx
@@ -1,19 +1,45 @@
 #pragma once
 
-#include "model.hxx"
 #include "view.hxx"
 
 #include <ge211.hxx>
 
-class Controller : public ge211::Abstract_game
+struct Controller : ge211::Abstract_game
 {
-public:
-    Controller();
+    //
+    // CONSTRUCTOR
+    //
 
-protected:
-    void draw(ge211::Sprite_set& set) override;
+    explicit Controller(Model&);
 
-private:
-    Model model_;
-    View view_;
+
+    //
+    // MEMBER FUNCTIONS
+    void on_key_up(ge211::Key) override;
+
+    /// Defines how the game responds to mouse clicks (which is by
+    /// telling the model to launch the ball).
+    void on_mouse_up(ge211::Mouse_button, ge211::Posn<int>) override;
+
+    void on_frame(double dt) override;
+
+    ge211::Dims<int> initial_window_dimensions() const override;
+
+    void draw(ge211::Sprite_set&) override;
+
+
+    //
+    // MEMBER VARIABLES
+    //
+
+    /// This is a reference to the model. It means that the controller
+    /// doesn't own the model but has access to it. Thus, the client of
+    /// the controller (brick_out.cxx) must create a `Model` first and
+    /// then pass that by reference to the `Controller` constructor.
+    Model& model;
+
+    /// This is the view, which contains the game’s data and code for
+    /// displaying the state of the model to the screen.
+    View view;
+    std::string player_input;
 };
