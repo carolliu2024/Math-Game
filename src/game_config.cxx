4327d211496825cebab994a308d09f46a9bb90ef
carolliu2024 <75544065+carolliu2024@users.noreply.github.com>
2021-12-30 00:31:53 -0600

    default values

--- src/game_config.cxx
+++ src/game_config.cxx
@@ -1,9 +1,14 @@
 #include "game_config.hxx"
 
+// Default values of all the member variables.
 Game_config::Game_config()
         : scene_dims {1024, 768},
-          velocity_0 {0, 50},
+          screen_bottom{scene_dims.height-50},
+          block_dims {150, 50},
+          restart_dims {150, 100},
+          block_velocity_0 {0, 20}, //Initial block fall velocity
+          top_margin {100},
           side_margin {170},
-          bottom_margin {500},
-
+          brick_depth {300},
+          bottom_margin {50}
 { }
